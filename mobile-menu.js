944f6c88a39119028e576b2a981a925592b56bb5
sflomenb <sam.flomenberg@gmail.com>
2017-01-04 16:23:09 -0500

    Introduce new single-page design and teal theme
    First and foremost, this includes a new single-page design. All of the
    content is now on index.html. This should make navigating easier.
    
    Instead of the white colors, there is now a teal theme.
    
    There is a custom nav-bar with some JS to expand and close the menu
    on mobile.

---
+++ mobile-menu.js
@@ -0,0 +1,7 @@
+// debugger;
+document.getElementById('menu-toggle').addEventListener('click', (ev) => {
+  // debugger;
+  console.log(ev);
+  document.getElementById("hamburger").classList.toggle('hide');
+  document.getElementById("close").classList.toggle('hide');
+})
