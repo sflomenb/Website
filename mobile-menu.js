6841e56c8b501920c7bc18bd60112e92109876ae
sflomenb <sam.flomenberg@gmail.com>
2017-01-05 11:30:26 -0500

    Add brand to mobile

--- mobile-menu.js
+++ mobile-menu.js
@@ -1,7 +1,4 @@
-// debugger;
 document.getElementById('menu-toggle').addEventListener('click', (ev) => {
-  // debugger;
-  console.log(ev);
   document.getElementById("hamburger").classList.toggle('hide');
   document.getElementById("close").classList.toggle('hide');
 })
